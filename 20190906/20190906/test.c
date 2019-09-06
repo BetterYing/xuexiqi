dcad1365cc8831350425fa6feaf0c45eae0fc8a9
BetterYing <876264306@qq.com>
2019-09-06 22:29:00 +0800

    turn

---
+++ 20190906/20190906/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+
+int main()
+{
+	char strget[50], strA[20] = "", strB[20] = "";
+	int a, i, index, nall, num, result = 1;
+	scanf("%s", strget);
+	nall = strlen(strget);
+	for (i = 0; i<nall; i++){
+		if (strget[i] == ';'){
+			a = i;
+			break;
+		}
+	}
+	for (i = 0; i<a; i++){
+		strA[i] = strget[i];
+		strB[i] = strget[nall - a + i];
+	}
+	num = strlen(strA);
+	// printf("%s\n%s\n",strA,strB);
+	for (i = 0; i<num; i++){
+		if (strA[i] == strB[0]){
+			index = i;
+			break;
+		}
+	}
+	// printf("%d\n",index);
+	for (i = 0; i<index; i++){
+		if (strA[i] != strB[num + i - index] || strA[num + i - index] != strB[i]){
+			result = 0;
+
+			break;
+		}
+	}
+	if (result)
+		printf("true\n");
+	else
+	{
+		printf("false\n");
+	}
+
+	system("pause");
+	return 0;
+}
+
