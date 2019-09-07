6ac3f7c32b469fc0acea91368628f1b08d437c19
BetterYing <876264306@qq.com>
2019-09-07 11:47:25 +0800

    swap

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c=0;
+        c=a;
+        a=b;
+        b=c;
+     System.out.println(a);
+     System.out.println(b);
+
+    }
+}
