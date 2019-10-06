0f6c8cea2fe09b0cd65d0f4ca8e78d8ff1b84363
BetterYing <53933815+BetterYing@users.noreply.github.com>
2019-10-06 17:34:21 +0800

    Update contact.c

--- 20190823/20190823/contact.c
+++ 20190823/20190823/contact.c
@@ -25,17 +25,13 @@ int add_contact(pContact pcon){
 int del_contact(pContact pcon){
 	int i = 0;
 	int rel = search_contact(pcon);
-	if (rel != -1)
-	{
-		for (i = rel; i<pcon->count_user - 1; i++)
-		{
+	if (rel != -1){
+		for (i = rel; i<pcon->count_user - 1; i++){
 			pcon->people[i] = pcon->people[i + 1];
 		}
 		pcon->count_user--;
 		return 1;
-	}
-	else
-	{
+	} else {
 		printf("不存在！\n");
 		return -1;
 	}
@@ -46,10 +42,8 @@ int search_contact(pContact pcon){
 	char name[NAME_LIGHT];
 	printf("请输入要查找的姓名：");
 	scanf("%s", name);
-	for (i = 0; i < pcon->count_user; i++)
-	{
-		if (strcmp(pcon->people[i].name, name) == 0)
-		{
+	for (i = 0; i < pcon->count_user; i++){
+		if (strcmp(pcon->people[i].name, name) == 0){
 			printf("\tname\tsex\tage\ttel\tadd\n");
 			printf("%10s", pcon->people[i].name);
 			printf("%5s", pcon->people[i].sex);
@@ -60,13 +54,11 @@ int search_contact(pContact pcon){
 		}
 	}
 	return -1;
-
 }
 
 int modify_contact(pContact pcon){
 	int rel = search_contact(pcon);
-	if (rel != -1)
-	{
+	if (rel != -1){
 		printf("请输入姓名：");
 		scanf("%s", pcon->people[pcon->count_user].name);
 		printf("请输入性别：");
@@ -78,9 +70,7 @@ int modify_contact(pContact pcon){
 		printf("请输入地址：");
 		scanf("%s", pcon->people[pcon->count_user].add);
 		return 1;
-	}
-	else
-	{
+	} else {
 		printf("输入错误!\n");
 		return -1;
 	}
@@ -89,8 +79,7 @@ int modify_contact(pContact pcon){
 int show_contact(pContact pcon){
 	int i;
 	printf("\tname\tsex\tage\ttel\tadd\n");
-	for (i = 0; i < pcon->count_user; i++)
-	{
+	for (i = 0; i < pcon->count_user; i++) {
 		printf("%10s", pcon->people[i].name);
 		printf("%5s", pcon->people[i].sex);
 		printf("%10d", pcon->people[i].age);
@@ -99,11 +88,9 @@ int show_contact(pContact pcon){
 	}
 	printf("\n");
 	return 0;
-	
 }
 
-int clear_contact(pContact pcon)
-{
+int clear_contact(pContact pcon){
 	pcon->count_user = 0;
 	return 1;
 }
